e9ed26f730c3b1a66e68715bc765bd4e9894c5b5
RoziePlaysPython <tfatugava@gmail.com>
2022-03-03 01:10:15 +0300

    git committing is a weird system. Anyway, APItoken is now passed with command line arguments

--- movie_searcher.py
+++ movie_searcher.py
@@ -1,7 +1,9 @@
 #You can find this bot here: https://t.me/vmeste_tv_bot
-#It probably works right now
-#It also searches movies on a russian website.
+#It probably doesn't work right now
+#It searches movies on a russian website.
+#UPDATE: It definitely doesn't work since vmeste.tv is blocked in Russia
 
+from sys import argv
 from lxml import html
 import requests
 from time import sleep
@@ -12,32 +14,43 @@ from functools import lru_cache
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-APItoken = "MYAPITOKEN"
+import TokenValidityCheck
+if TokenValidityCheck.CheckForAPIKey():
+    APItoken = argv[1]
 
 from telegram.ext import Updater
 upd = Updater(token=APItoken, use_context=True)
 disp = upd.dispatcher
 import logging
-logging.basicConfig(format='8==>%(asctime)s - %(name)s - %(levelname)s - %(message)s<==8',
+logging.basicConfig(format='>%(asctime)s - %(name)s - %(levelname)s - %(message)s<',
                      level=logging.INFO)
-#8==>i am funny<==8
 
 default_href="https://vmeste.tv/search/?text="
-headers={"User Agent" : "telegram bot (made by https://t.me/Riley_Lvasture_Ka_Linux)"}
+headers={"User Agent" : "telegram bot (made by https://t.me/rilvkali)"}
 
-
-#I know, using class here is stoopid and pointless, I'm just too dumb to change that
-class kbMarkup:
-    def __init__(self, hrefs, current_page):
-        self.button_list = list()
-        self.columns = 5
-        for num in range(len(hrefs)):
-            s = str(num+1)
-            if num==current_page:
-                s = f'<{s}>'
-            self.button_list.append(InlineKeyboardButton(s, callback_data = str(num)))
-        self.button_list = [self.button_list[i: i+self.columns] for i in range(0, len(hrefs), self.columns)]
-        self.keyboard = InlineKeyboardMarkup(self.button_list)
+##Gotta rewrite that class into a func and hope nothing breaks
+#class kbMarkup:
+#    def __init__(self, hrefs, current_page):
+#        self.button_list = list()
+#        self.columns = 5
+#        for num in range(len(hrefs)):
+#            s = str(num+1)
+#            if num==current_page:
+#                s = f'<{s}>'
+#            self.button_list.append(InlineKeyboardButton(s, callback_data = str(num)))
+#        self.button_list = [self.button_list[i: i+self.columns] for i in range(0, len(hrefs), self.columns)]
+#        self.keyboard = InlineKeyboardMarkup(self.button_list)
+def kbMarkup(hrefs, current_page):
+    button_list = list()
+    columns = 5
+    for num in range (len(hrefs)):
+        s = str(num+1)
+        if num==current_page:
+            s = f'<{s}>'
+        button_list.append(InlineKeyboardButton(s, callback_data = str(num)))
+        button_list = [button_list[i: i+columns] for i in range(0, len(hrefs), columns)]
+        keyboard = InlineKeyboardMarkup(button_list)
+        return keyboard
 
 #This fixes the issue with long answering 
 #and making requests to the site every time you press button
@@ -57,7 +70,7 @@ def searcher(update, context):
     result = context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=get_href(context.user_data['current_search'])[0],
-        reply_markup = kbMarkup(get_href(context.user_data['current_search']), 0).keyboard
+        reply_markup = kbMarkup(get_href(context.user_data['current_search']), 0)
     )
     context.user_data['current_message_id'] = result.message_id
 
@@ -73,7 +86,7 @@ def callback_catcher(update, context):
     result = context.bot.edit_message_reply_markup(
         chat_id=update.effective_chat.id,
         message_id = context.user_data['current_message_id'],
-        reply_markup = kbMarkup(get_href(context.user_data['current_search']), page).keyboard
+        reply_markup = kbMarkup(get_href(context.user_data['current_search']), page)
     )
     context.user_data['current_message_id'] = result.message_id
 
